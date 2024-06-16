c91046d63807a0e6311bb6a0db433af7d1ebe1de
root <root@localhost.localdomain>
2024-06-16 22:18:56 +0800

    jar genrate and use

---
+++ jarDemo/CallerDemo/Caller.java
@@ -0,0 +1,8 @@
+import myclasspackage.MyClass;
+
+public class Caller{
+    public static void main(String[] args){
+        MyClass myclass = new MyClass();
+        myclass.greet("Alice");
+    }
+}
